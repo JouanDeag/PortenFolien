4fe5cf1c69c05e944a7e95509788a9fa06fea655
Johan Karlsson <johan.k0528@gmail.com>
2023-03-22 11:12:25 +0100

    first commit

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+// Add nav.html to the DOM
+const navHTML = fetch('assets/components/nav/nav.html').then(response => response.text());
+navHTML.then(html => document.querySelector('body').insertAdjacentHTML('afterbegin', html));
+
+const fem = `______ _____ ______ __  __            _____ 
+|  ____|_   _|  ____|  \\\/  |          | ____|
+| |__    | | | |__  | \\  / |  ______  | |__  
+|  __|   | | |  __| | |\\\/| | |______| |___ \\ 
+| |     _| |_| |____| |  | |           ___) |
+|_|    |_____|______|_|  |_|          |____/                                 
+`
+
+const showFem = () => {
+    console.log(fem);
+}
+
+showFem();
+
+/* Handle the bouncing animation of the hello text */
+
+// Get the text to animate
+const hello = document.querySelector('#hello');
+
+// Create an array of spans with the letters of the text
+elementsToAdd = [...hello.innerText].map((letter, index) => `<span class="bouncing" style="animation-delay: ${index * 50}ms;">${letter}</span>`).join('');
+
+// Add the spans to the DOM
+hello.innerHTML = elementsToAdd;
+
